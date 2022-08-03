13c823c9940080fcbbff3c7b811a26d5a4fad659
Saurabh <raisaurabh265@gmail.com>
2022-08-03 18:37:54 +0530

    files separated

--- src/App.js
+++ src/App.js
@@ -1,49 +1,10 @@
 import "./App.css";
-import { useEffect, useState } from "react";
-import { useRef } from "react";
-
+import Input from "./components/Input";
 function App() {
-  const [floorNumberByUserInput, setFloorNumberByUserInput] = useState([]);
-  const [generatedFloors, setGeneratedFloors] = useState([]);
-  const inputRef = useRef(null);
-  const generateFloor = () => {
-    if (inputRef.current.value === "") {
-      alert("Please enter a number");
-    }
-    if (inputRef.current.value <= 0) {
-      alert("These lifts don't go down");
-    }
-    setFloorNumberByUserInput(inputRef.current.value);
-  };
-
-  useEffect(() => {
-    setGeneratedFloors(
-      Array.from({ length: floorNumberByUserInput }, (_, index) => index + 1)
-    );
-  }, [floorNumberByUserInput]);
-
   return (
     <div className="App">
-      <header className="">hello lift </header>
-      <input type="number" ref={inputRef} />
-
-      <button onClick={generateFloor}>add floor</button>
-      {generatedFloors
-        .map((floor, index) => {
-          return (
-            <div key={index}>
-              <p
-                style={{
-                  backgroundColor: "red",
-                  color: "white",
-                }}
-              >
-                floor{index + 1}
-              </p>
-            </div>
-          );
-        })
-        .reverse()}
+      <header className="">Lift Simulation</header>
+      <Input />
     </div>
   );
 }
---
+++ src/components/Input.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { useEffect, useState } from "react";
+import { useRef } from "react";
+function Input() {
+  const [floorNumberByUserInput, setFloorNumberByUserInput] = useState([]);
+  const [generatedFloors, setGeneratedFloors] = useState([]);
+  const inputRef = useRef(null);
+  const generateFloor = () => {
+    if (inputRef.current.value === "") {
+      alert("Please enter a number");
+    }
+    if (inputRef.current.value <= 0) {
+      alert("These lifts don't go down");
+    }
+    setFloorNumberByUserInput(inputRef.current.value);
+  };
+
+  useEffect(() => {
+    setGeneratedFloors(
+      Array.from({ length: floorNumberByUserInput }, (_, index) => index + 1)
+    );
+  }, [floorNumberByUserInput]);
+  return (
+    <div>
+      <input type="number" ref={inputRef} />
+
+      <button onClick={generateFloor}>add floor</button>
+      {generatedFloors
+        .map((floor, index) => {
+          return (
+            <div key={index}>
+              <p
+                style={{
+                  backgroundColor: "red",
+                  color: "white",
+                }}
+              >
+                floor{index + 1}
+              </p>
+            </div>
+          );
+        })
+        .reverse()}
+    </div>
+  );
+}
+
+export default Input;
